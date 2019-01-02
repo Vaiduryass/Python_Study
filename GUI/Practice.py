12e13375d7ee5f8fe41568b230c8d20f4571a1b0
Vaidurya <1756487272@qq.com>
2019-01-02 16:41:16 +0800

    nothing

--- GUI/Practice.py
+++ GUI/Practice.py
@@ -13,8 +13,38 @@ class GUI(Tk):
         self.canvas = Canvas(self, width=495, height=305, bg='#000000')
         self.canvas.pack()
         self.snake = self.canvas.create_line((0,0),(0,0), fill='#FFFF00', width=10)
-        self.food = self.canvas.create_rectangle(0,0,0,0, fill='#00FF00', outline='#00FF00')
+        self.food = self.canvas.create_rectangle(0,0, fill='#00FF00', outline='#00FF00')
         self.point_score = self.canvas.create_text(455, 15, fill='white', text='score:0')
+    def restart(self):
+        self.destroy()
+        main()
+    def queue_handler(self):
+        try:
+            while True:
+                task = self.queue.get(block=False)
+                if task.get('game_over'):
+                    self.game_over()
+                elif task.get('move'):
+                    points = [x for point in task['move'] for x in point]
+                    self.canvas.coords(self.snake, *points)
+                elif task.get('food'):
+                    self.canvas.coords(self.food, *task['food'])
+                elif task.get('points_score'):
+                    self.canvas.itemconfigure(self.point_score,
+                                              text='score:{}'.format(task['points_score']))
+                    self.queue.task_done()
+        except queue.Empty:
+            if not self.is_game_over:
+                self.canvas.after(100, self.queue_handler)
+
+    def game_over(self):
+        self.is_game_over = True
+        self.canvas.create_text(220, 150, fill='white',text='Game Over!')
+        quitbtn = Button(self, text='Quit', command=self.destroy)
+        retbtn = Button(self, text='Resume', command=self.restart)
+        self.canvas.create_window(230, 180, anchor=W, window=quitbtn)
+        self.canvas.create_window(200, 180, anchor=E, window=retbtn)
+
 
 
 
