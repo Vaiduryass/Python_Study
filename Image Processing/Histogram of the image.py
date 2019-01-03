d290889d0d6f14997931a460d970d6dbe77e4873
Vaidurya <1756487272@qq.com>
2019-01-03 16:38:21 +0800

    little

--- Image Processing/Histogram of the image.py
+++ Image Processing/Histogram of the image.py
@@ -1,4 +1,4 @@
-
+'''
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,3 +14,20 @@ plt.hist(ag, bins=256, density=1, facecolor='g',edgecolor='g')
 ab=np.array(b).flatten()
 plt.hist(ab, bins=256, density=1, facecolor='b',edgecolor='b')
 plt.show()
+'''
+
+from PIL import Image
+from pylab import *
+# 读取图像到数组中
+im = array(Image.open('Infrared.jpg').convert('L'))
+# 新建一个图像
+figure()
+# 不使用颜色信息
+gray()
+# 在原点的左上角显示轮廓图像
+contour(im, origin='image')
+axis('equal')
+axis('off')
+figure()
+hist(im.flatten(),128)
+show()
---
+++ Image Processing/_Draw.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import numpy as np
+import pylab 
+# 读取图像到数组中
+im = np.array(Image.open('C:\\Users\\vaiduryass\\Desktop\\Infrared.jpg'))
+# 绘制图像
+pylab.imshow(im)
+# 一些点
+x = [100,100,400,400]
+y = [200,500,200,500]
+# 使用红色星状标记绘制点
+pylab.plot(x,y,'r*')
+# 绘制连接前两个
+# 添加标题，显示绘制的图像
+pylab.title('Plotting: "empire.jpg"')
+pylab.show()
